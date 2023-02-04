2a6c337c99df88017328194be7efbb29b5d8416c
sjorgy3 <87779735+sjorgy3@users.noreply.github.com>
2023-02-03 17:26:37 -0700

    hmmm

--- src/spell/Main.java
+++ src/spell/Main.java
@@ -1,6 +1,10 @@
 package spell;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+import java.util.Scanner;
 
 /**
  * A simple main class for running the spelling corrector. This class is not
@@ -13,14 +17,40 @@ public class Main {
 	 * as the second argument.
 	 */
 	public static void main(String[] args) throws IOException {
-		
+
 		String dictionaryFileName = args[0];
 		String inputWord = args[1];
-		
+		//todo Finish writing the code to put all the words in a hashmap with the correct size.
+
+		HashMap wordMap = new HashMap();
+		File dictionary = new File(dictionaryFileName);
+		Scanner sc = new Scanner(dictionary);
+
+
+		while(sc.hasNextLine()){
+			String word = sc.nextLine();
+			if(wordMap.containsKey(word)){
+				System.out.println(word);
+				wordMap.replace(word,)s
+			}
+
+
+		}
+
+
+
+
+
+
+
+
+
+
 		//
         //Create an instance of your corrector here
         //
 		ISpellCorrector corrector = null;
+
 		
 		corrector.useDictionary(dictionaryFileName);
 		String suggestion = corrector.suggestSimilarWord(inputWord);
